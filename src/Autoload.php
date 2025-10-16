ac02394130abb7747a836cb937b62424512a916e
Michael Snoeren <hello@michaelsnoeren.nl>
2025-10-16 08:03:46 +0000

    Improvements

--- src/Autoload.php
+++ src/Autoload.php
@@ -3,29 +3,50 @@ declare(strict_types = 1);
 
 pest()->presets()->custom('r2h', fn (): array => [
     // Controllers should not extend any base controller.
-    expect('App\Http\Controllers')->toExtendNothing(),
+    expect('App\Http\Controllers')
+        ->toBeClasses()
+        ->toExtendNothing(),
 
     // Policies should not use the HandlesAuthorization trait.
-    expect('App\Policies')->not->toUseTraits('Illuminate\Auth\Access\HandlesAuthorization'),
+    expect('App\Policies')
+        ->toBeClasses()
+        ->not->toUseTraits('Illuminate\Auth\Access\HandlesAuthorization'),
 
     // Notifications should be queueable at any time.
-    expect('App\Notifications')->toImplement('Illuminate\Contracts\Queue\ShouldQueue'),
+    expect('App\Notifications')
+        ->toBeClasses()
+        ->toImplement('Illuminate\Contracts\Queue\ShouldQueue'),
 
     // Listeners must have a suffix and a handle method.
-    expect('App\Listeners')->toHaveSuffix('Listener')->toHaveMethod('handle'),
+    expect('App\Listeners')
+        ->toBeClasses()
+        ->toHaveSuffix('Listener')
+        ->toHaveMethod('handle'),
 
     // Actions must have a suffix and a handle method.
-    expect('App\Actions')->toHaveSuffix('Action')->toHaveMethod('handle'),
+    expect('App\Actions')
+        ->toBeClasses()
+        ->toHaveSuffix('Action')
+        ->toHaveMethod('handle'),
 
     // Jobs must have a suffix.
-    expect('App\Jobs')->toHaveSuffix('Job'),
+    expect('App\Jobs')
+        ->toBeClasses()
+        ->toHaveSuffix('Job'),
 
     // Any contracts must be an interface.
     expect('App\Contracts')->toBeInterfaces(),
 
-    // Services must have a suffix.
-    expect('App\Services')->toHaveSuffix('Service'),
-
     // Data classes must have a suffix.
-    expect('App\Data')->toHaveSuffix('Data'),
+    expect('App\Data')
+        ->toBeClasses()
+        ->toHaveSuffix('Data'),
+
+    // Enums must be enums.
+    expect('App\Enums')->toBeEnums(),
+
+    // Observers must have a suffix.
+    expect('App\Observers')
+        ->toBeClasses()
+        ->toHaveSuffix('Observer'),
 ]);
